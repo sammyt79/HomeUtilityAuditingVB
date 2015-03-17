10225611ed51acbc66f5ec4c5a1f591e487337ec
Samuel Tollefson <sam8879@gmail.com>
2015-03-16 22:40:32 -0400

    .NET1

---
+++ Home Utility Auditing Program/Home Utility Auditing Program/MainForm.Designer.vb
@@ -0,0 +1,231 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtCost = New System.Windows.Forms.TextBox()
+        Me.cboAppliance = New System.Windows.Forms.ComboBox()
+        Me.lblAppliance = New System.Windows.Forms.Label()
+        Me.lblCost = New System.Windows.Forms.Label()
+        Me.lblPower = New System.Windows.Forms.Label()
+        Me.lblHours = New System.Windows.Forms.Label()
+        Me.txtPower = New System.Windows.Forms.TextBox()
+        Me.txtHours = New System.Windows.Forms.TextBox()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.txtTotalHr = New System.Windows.Forms.TextBox()
+        Me.txtTotalYr = New System.Windows.Forms.TextBox()
+        Me.lblHour = New System.Windows.Forms.Label()
+        Me.lblYear = New System.Windows.Forms.Label()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtCost
+        '
+        Me.txtCost.Location = New System.Drawing.Point(183, 67)
+        Me.txtCost.Name = "txtCost"
+        Me.txtCost.Size = New System.Drawing.Size(50, 20)
+        Me.txtCost.TabIndex = 0
+        '
+        'cboAppliance
+        '
+        Me.cboAppliance.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboAppliance.FormattingEnabled = True
+        Me.cboAppliance.Items.AddRange(New Object() {"Stereo", "Dish Washer", "Lava Lamp", "Microwave", "Gamma Blaster", "TV"})
+        Me.cboAppliance.Location = New System.Drawing.Point(183, 22)
+        Me.cboAppliance.Name = "cboAppliance"
+        Me.cboAppliance.Size = New System.Drawing.Size(121, 21)
+        Me.cboAppliance.TabIndex = 1
+        '
+        'lblAppliance
+        '
+        Me.lblAppliance.AutoSize = True
+        Me.lblAppliance.Location = New System.Drawing.Point(24, 25)
+        Me.lblAppliance.Name = "lblAppliance"
+        Me.lblAppliance.Size = New System.Drawing.Size(85, 13)
+        Me.lblAppliance.TabIndex = 2
+        Me.lblAppliance.Text = "Home Appliance"
+        '
+        'lblCost
+        '
+        Me.lblCost.AutoSize = True
+        Me.lblCost.Location = New System.Drawing.Point(24, 70)
+        Me.lblCost.Name = "lblCost"
+        Me.lblCost.Size = New System.Drawing.Size(80, 13)
+        Me.lblCost.TabIndex = 3
+        Me.lblCost.Text = "Cost / kW-hour"
+        '
+        'lblPower
+        '
+        Me.lblPower.AutoSize = True
+        Me.lblPower.Location = New System.Drawing.Point(24, 115)
+        Me.lblPower.Name = "lblPower"
+        Me.lblPower.Size = New System.Drawing.Size(109, 13)
+        Me.lblPower.TabIndex = 4
+        Me.lblPower.Text = "Power Needed in kW"
+        '
+        'lblHours
+        '
+        Me.lblHours.AutoSize = True
+        Me.lblHours.Location = New System.Drawing.Point(24, 160)
+        Me.lblHours.Name = "lblHours"
+        Me.lblHours.Size = New System.Drawing.Size(93, 13)
+        Me.lblHours.TabIndex = 5
+        Me.lblHours.Text = "Hours Used / Day"
+        '
+        'txtPower
+        '
+        Me.txtPower.Location = New System.Drawing.Point(183, 112)
+        Me.txtPower.Name = "txtPower"
+        Me.txtPower.Size = New System.Drawing.Size(50, 20)
+        Me.txtPower.TabIndex = 6
+        '
+        'txtHours
+        '
+        Me.txtHours.Location = New System.Drawing.Point(183, 157)
+        Me.txtHours.Name = "txtHours"
+        Me.txtHours.Size = New System.Drawing.Size(50, 20)
+        Me.txtHours.TabIndex = 7
+        '
+        'btnClear
+        '
+        Me.btnClear.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold)
+        Me.btnClear.Location = New System.Drawing.Point(287, 64)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 25)
+        Me.btnClear.TabIndex = 8
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold)
+        Me.btnCalculate.Location = New System.Drawing.Point(287, 109)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 25)
+        Me.btnCalculate.TabIndex = 9
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold)
+        Me.btnExit.Location = New System.Drawing.Point(287, 154)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 25)
+        Me.btnExit.TabIndex = 10
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'txtTotalHr
+        '
+        Me.txtTotalHr.Location = New System.Drawing.Point(200, 208)
+        Me.txtTotalHr.Name = "txtTotalHr"
+        Me.txtTotalHr.ReadOnly = True
+        Me.txtTotalHr.Size = New System.Drawing.Size(45, 20)
+        Me.txtTotalHr.TabIndex = 11
+        '
+        'txtTotalYr
+        '
+        Me.txtTotalYr.Location = New System.Drawing.Point(294, 208)
+        Me.txtTotalYr.Name = "txtTotalYr"
+        Me.txtTotalYr.ReadOnly = True
+        Me.txtTotalYr.Size = New System.Drawing.Size(70, 20)
+        Me.txtTotalYr.TabIndex = 13
+        '
+        'lblHour
+        '
+        Me.lblHour.AutoSize = True
+        Me.lblHour.Location = New System.Drawing.Point(248, 215)
+        Me.lblHour.Name = "lblHour"
+        Me.lblHour.Size = New System.Drawing.Size(38, 13)
+        Me.lblHour.TabIndex = 14
+        Me.lblHour.Text = "/ Hour"
+        '
+        'lblYear
+        '
+        Me.lblYear.AutoSize = True
+        Me.lblYear.Location = New System.Drawing.Point(366, 215)
+        Me.lblYear.Name = "lblYear"
+        Me.lblYear.Size = New System.Drawing.Size(37, 13)
+        Me.lblYear.TabIndex = 15
+        Me.lblYear.Text = "/ Year"
+        '
+        'lblTotal
+        '
+        Me.lblTotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold)
+        Me.lblTotal.Location = New System.Drawing.Point(24, 211)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.lblTotal.Size = New System.Drawing.Size(170, 13)
+        Me.lblTotal.TabIndex = 16
+        Me.lblTotal.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'MainForm
+        '
+        Me.AcceptButton = Me.btnCalculate
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnClear
+        Me.ClientSize = New System.Drawing.Size(432, 260)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.lblYear)
+        Me.Controls.Add(Me.lblHour)
+        Me.Controls.Add(Me.txtTotalYr)
+        Me.Controls.Add(Me.txtTotalHr)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.txtHours)
+        Me.Controls.Add(Me.txtPower)
+        Me.Controls.Add(Me.lblHours)
+        Me.Controls.Add(Me.lblPower)
+        Me.Controls.Add(Me.lblCost)
+        Me.Controls.Add(Me.lblAppliance)
+        Me.Controls.Add(Me.cboAppliance)
+        Me.Controls.Add(Me.txtCost)
+        Me.Name = "MainForm"
+        Me.Text = "Home Utility Auditing Program"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtCost As System.Windows.Forms.TextBox
+    Friend WithEvents cboAppliance As System.Windows.Forms.ComboBox
+    Friend WithEvents lblAppliance As System.Windows.Forms.Label
+    Friend WithEvents lblCost As System.Windows.Forms.Label
+    Friend WithEvents lblPower As System.Windows.Forms.Label
+    Friend WithEvents lblHours As System.Windows.Forms.Label
+    Friend WithEvents txtPower As System.Windows.Forms.TextBox
+    Friend WithEvents txtHours As System.Windows.Forms.TextBox
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents txtTotalHr As System.Windows.Forms.TextBox
+    Friend WithEvents txtTotalYr As System.Windows.Forms.TextBox
+    Friend WithEvents lblHour As System.Windows.Forms.Label
+    Friend WithEvents lblYear As System.Windows.Forms.Label
+    Friend WithEvents lblTotal As System.Windows.Forms.Label
+
+End Class
---
+++ Home Utility Auditing Program/Home Utility Auditing Program/MainForm.vb
@@ -0,0 +1,146 @@
+﻿' Home Utility Auditing Program
+' Samuel Tollefson
+' POS/408
+' March 9, 2015
+' Tim Hagan
+
+Public Class MainForm
+
+    Dim dblCost As Double ' Variable to hold txtCost input.
+    Dim dblPower As Double ' Variable to hold txtPower input.
+    Dim dblHours As Double ' Variable to hold txtHours input.
+    Dim dblTotalHr As Double ' Variable to hold txtTotalHr output.
+    Dim dblTotalYr As Double ' Variable to hold txtTotalYr output.
+    Dim allGood As Boolean = True ' Used to test if the program is ready for calculations.
+
+    ' Determine what happens when the user presses the "Clear" button (btnClear).
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+        ' Reset combo box.
+        cboAppliance.SelectedIndex = -1
+
+        ' Clear input text boxes.
+        txtCost.Clear()
+        txtPower.Clear()
+        txtHours.Clear()
+
+        ' Clear output text boxes.
+        ClearOutputs()
+
+    End Sub
+
+    ' Determine what happens when the user presses the "Calculate" button (btnCalculate).
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+
+        allGood = True ' Ensure variable is set to true before inputs are read.
+
+        ' Test combobox for valid user selection.
+        If cboAppliance.SelectedIndex = -1 Then
+
+            ' Display error message.
+            MsgBox("Please select an appliance", , " input error")
+            allGood = False ' Not ready for calculations.
+        End If
+
+        ' Validate that the cost entered has the correct format and is within a reasonable range.
+        Try
+            dblCost = CDbl(txtCost.Text)
+            If dblCost < 0.05 Or dblCost > 0.5 Then
+
+                ' Display error message.
+                MsgBox("Cost / kW-hour must be between " + FormatCurrency("0.05") + "and " + FormatCurrency("0.5"), , " input error")
+                txtCost.Clear() ' Clear cost input textbox.
+                allGood = False ' Not ready for calculations.
+            End If
+        Catch ex As Exception
+
+            ' Display error message.
+            MsgBox("Please enter a valid amount for amount for ""Cost""", , " input error")
+            txtCost.Clear() ' Clear cost input textbox.
+            allGood = False ' Not ready for calculations.
+        End Try
+
+        ' Validate that the power entered has the correct format and is within a reasonable range.
+        Try
+            dblPower = CDbl(txtPower.Text)
+            If dblPower < 1 Or dblPower > 100 Then
+
+                ' Display error message.
+                MsgBox("The value for Power Needed must be between 1.0 and 100", , " input error")
+                txtPower.Clear() ' Clear power input textbox.
+                allGood = False ' Not ready for calculations.
+            End If
+        Catch ex As Exception
+
+            ' Display error message.
+            MsgBox("Please enter a valid amount for ""Power Needed""", , " input error")
+            txtPower.Clear() ' Clear power input textbox.
+            allGood = False ' Not ready for calculations.
+        End Try
+
+        ' Validate that the hours entered has the correct format and is within a reasonable range.
+        Try
+            dblHours = CDbl(txtHours.Text)
+            If dblHours < 0 Or dblHours > 24 Then
+
+                ' Display error message.
+                MsgBox("Hours Used must be within a 24 hour period", , " input error")
+                txtHours.Clear() ' Clear hours input Textbox.
+                allGood = False ' Not ready for calculations.
+            End If
+        Catch ex As Exception
+
+            ' Display error message.
+            MsgBox("Please enter a valid amount for ""Hours Used""", , " input error")
+            txtHours.Clear() ' Clear hours input Textbox.
+            allGood = False ' Not ready for calculations.
+        End Try
+
+        ' Check to see if inputs have been validated.
+        If allGood = True Then
+
+            ' Display output message.
+            lblTotal.Text = cboAppliance.SelectedItem() + "'s total cost ="
+
+            ' Calculate totals.
+            dblTotalHr = dblCost * dblPower
+            dblTotalYr = dblTotalHr * dblHours * 365
+
+            ' Display totals.
+            txtTotalHr.Text = FormatCurrency(dblTotalHr)
+            txtTotalYr.Text = FormatCurrency(dblTotalYr)
+        End If
+
+    End Sub
+
+    ' Determine what happens when the user presses the "Exit" button (btnExit).
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close() ' Close the program.
+    End Sub
+
+    ' Clear outputs when user changess selection.
+    Private Sub cboAppliance_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboAppliance.SelectedIndexChanged
+        ClearOutputs()
+    End Sub
+
+    ' Clear outputs when user changess input.
+    Private Sub txtCost_TextChanged(sender As Object, e As EventArgs) Handles txtCost.TextChanged
+        ClearOutputs()
+    End Sub
+
+    ' Clear outputs when user changess input.
+    Private Sub txtPower_TextChanged(sender As Object, e As EventArgs) Handles txtPower.TextChanged
+        ClearOutputs()
+    End Sub
+
+    ' Clear outputs when user changess input.
+    Private Sub txtHours_TextChanged(sender As Object, e As EventArgs) Handles txtHours.TextChanged
+        ClearOutputs()
+    End Sub
+
+    ' This procedure clears the output text boxes.
+    Sub ClearOutputs()
+        txtTotalHr.Clear()
+        txtTotalYr.Clear()
+    End Sub
+End Class
